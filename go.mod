74e0dba5ebb2a0e552fd93d640e1f2caef93c8d3
AlrksDess <20dess20@gmail.com>
2023-07-12 21:53:07 +0300

    Your commit message

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module osnova
 
 go 1.19
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println(1)
+
+}
+
+func init() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+
+	log.Println(port)
 }
